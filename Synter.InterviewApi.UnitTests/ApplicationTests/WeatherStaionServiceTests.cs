f8498dffc52b0772f96a51331a7a9b4f95b45740
Maria Porter <Maria.Porter@howdens.com>
2024-02-19 06:43:58 +0000

    Added response model test

---
+++ Synter.InterviewApi.UnitTests/ApplicationTests/WeatherStaionServiceTests.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Synter.InterviewApi.Application.Services;
+using Synter.InterviewApi.Application.Services.Interfaces;
+using Synter.InterviewApi.Domain.DataModels;
+using Synter.InterviewApi.Domain.RequestModels;
+using Synter.InterviewApi.Domain.ResponseModels;
+using Synter.InterviewApi.Infrastructure.Repositories.Interfaces;
+using Sytner.InterviewApi.Controllers;
+
+
+namespace Synter.InterviewApi.UnitTests.ApplicationTests
+{
+    public class WeatherStaionServiceTests
+    {
+        private const string stationName = "Corby";
+        private const int stationId = 1;
+        private const string stationCode = "WS001";
+        private const decimal lat = (decimal)-0.5;
+        private const decimal lon = (decimal)-0.5;
+
+
+        [Fact]
+        public void AddWeatherStation_ReturnsResult()
+        {
+            // Arrange
+            WeatherStationRequestModel requestModel = new()
+            {
+                StationName = stationName,
+                StationCode = stationCode,
+                Latitude = lat,
+                Longitude = lon
+            };
+
+            WeatherStation dbWeatherStation = new()
+            {
+                Name = stationName,
+                Code = stationCode,
+                Latitude = lat,
+                Longitude = lon,
+                Id = stationId,
+                CreatedDate = DateTime.UtcNow,
+                ModifiedDate = DateTime.UtcNow
+            };
+
+            var weatherStationRepoMock = new Mock<IWeatherStationRepository>();
+            weatherStationRepoMock.Setup(s => s.Add(dbWeatherStation));
+            var weatherStationService = new WeatherStationService(weatherStationRepoMock.Object);
+
+            // Act
+            var result = weatherStationService.AddWeatherStation(requestModel);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(result.Name, stationName);
+            Assert.Equal(result.Latitude, lat);
+            Assert.Equal(result.Longitude, lon);
+            Assert.Equal(DateTime.UtcNow.Day, result.CreatedDate.Day);
+            Assert.Equal(DateTime.UtcNow.Day, result.ModifiedDate.Day);
+        }
+
+    }
+}
---
+++ Synter.InterviewApi.UnitTests/DomainTests/WeatherForecastResponseModelTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xunit;
+using Synter.InterviewApi.Domain.Constants.Enums;
+using Synter.InterviewApi.Domain.ResponseModels;
+
+namespace Synter.InterviewApi.UnitTests.DomainTests
+{
+    public class WeatherForecastResponseModelTests
+    {
+        [Theory]
+        [InlineData(-5, WeatherSummary.Freezing)]
+        [InlineData(5, WeatherSummary.Chilly)]
+        [InlineData(15, WeatherSummary.Mild)]
+        [InlineData(25, WeatherSummary.Warm)]
+        [InlineData(35, WeatherSummary.Sweltering)]
+        [InlineData(45, WeatherSummary.Scorching)]
+        public void GetWeatherSummary_ReturnsCorrectSummary(int temperatureC, WeatherSummary expectedSummary)
+        {
+            // Arrange
+            var weatherForecast = new WeatherForecastResponseModel
+            {
+                Date = DateTime.Today,
+                TemperatureC = temperatureC
+            };
+
+            // Act
+            var actualSummary = weatherForecast.GetWeatherSummary(temperatureC);
+
+            // Assert
+            Assert.Equal(expectedSummary.ToString(), actualSummary);
+        }
+    }
+}
